f83072951f37301ad91c07ca086009d25c2b72c6
CongPham <congphamvan115@gmail.com>
2022-08-08 23:05:42 +0700

    8/8

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -35,10 +35,36 @@ let displayGetCRUD = async(req, res) => {
         dataTable:data
     })
 }
+let getEditCRUD = async (req, res) => {
+    let userId = req.query.id;
+ 
+    if (userId) {
+        let userData = await CRUDService.getUserInfoById(userId);
+
+
+        return res.render('editCRUD.ejs', {
+            user:userData
+        });
+        
+    }
+    else {
+        return res.send('ko tim thay');
+    }
+}
+
+let putCRUD = async (req , res) => {
+    let data = req.body;
+    let allUsers =await CRUDService.updateUserData(data);
+    return res.render('displayCRUD.ejs', {
+        dataTable:allUsers
+    })
+}
 module.exports = {
     getHomePage,
     getAboutPage,
     getCRUD,
     postCRUD,
     displayGetCRUD,
+    getEditCRUD,
+    putCRUD
 }
